ec8c9452d37545d88236e6587cb774fabe89bfc5
divyanshbhatt527 <divyanshbhatt527@gmail.com>
2024-10-12 20:56:13 +0530

    modified database.py

--- app/database.py
+++ app/database.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
-
+DATABASE_NAME = os.getenv("DATABASE_NAME", "fastapi_db")
 client = AsyncIOMotorClient(MONGODB_URI)
-database = client.get_default_database()
+database = client[DATABASE_NAME]
 
 def serialize_mongo(item):
     item["id"] = str(item["_id"])
