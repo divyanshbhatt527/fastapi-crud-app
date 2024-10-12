a44f5de2e5b9a7ce66420a9f789ad29fcab5fc37
divyanshbhatt527 <divyanshbhatt527@gmail.com>
2024-10-12 19:59:16 +0530

    connected mongo cluster with project

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 
-client = AsyncIOMotorClient("mongodb://localhost:27017")  
+client = AsyncIOMotorClient("mongodb+srv://divyanshbhatt527:kwyb7UEdg2mUDRcS@cluster0.3u2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  
 database = client['fastapi_db']
 
 def serialize_mongo(item):
