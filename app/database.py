4ff05bd5c13891dff79613efffbad0c2aed6c80c
divyanshbhatt527 <divyanshbhatt527@gmail.com>
2024-10-12 20:07:49 +0530

    corrected env var error

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 
-MONGO_URI = os.getenv(AsyncIOMotorClient("mongodb+srv://divyanshbhatt527:kwyb7UEdg2mUDRcS@cluster0.3u2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+MONGO_URI = os.getenv("MONGO_URI","mongodb+srv://divyanshbhatt527:kwyb7UEdg2mUDRcS@cluster0.3u2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = AsyncIOMotorClient(MONGO_URI)
 database = client['fastapi_db']
 
 def serialize_mongo(item):
