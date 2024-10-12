cebd8f07cda903e2a58bd1d62bfed3cefb9d0eca
divyanshbhatt527 <divyanshbhatt527@gmail.com>
2024-10-12 20:06:32 +0530

    added env var for koyeb hosting

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 
-client = AsyncIOMotorClient("mongodb+srv://divyanshbhatt527:kwyb7UEdg2mUDRcS@cluster0.3u2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  
+MONGO_URI = os.getenv(AsyncIOMotorClient("mongodb+srv://divyanshbhatt527:kwyb7UEdg2mUDRcS@cluster0.3u2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 database = client['fastapi_db']
 
 def serialize_mongo(item):
